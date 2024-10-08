aaaf83b305653081e5b4440d8382376aafb0c10d
Adedoyin13 <olajideadedoyinrodiyat@gmail.com>
2024-10-08 09:35:46 +0100

    second commit

--- script.js
+++ script.js
@@ -4,7 +4,6 @@ const password2El = document.getElementById("password2");
 const messageContainer = document.querySelector(".message-container");
 const message = document.getElementById("message");
 
-
 let isValid = false;
 let passwordMatch = false;
 
@@ -12,11 +11,6 @@ const validateForm = () => {
     
     // Using Contraint API
     isValid = form.checkValidity();
-    // console.log(isValid);
-    // Style main message for an error
-    // message.textContent = "Please fill out all fields";
-    // message.style.color = "red";
-    // messageContainer.style.borderColor = "red"; we wrap everything with if and else
 
     if (!isValid) {
         message.textContent = "Please fill out all fields";
@@ -30,14 +24,14 @@ const validateForm = () => {
     if(password1El.value === password2El.value) {
         passwordMatch = true;
         password1El.style.borderColor = "green !important";
-        password2El.style.borderColor = "green !imortant";
+        password2El.style.borderColor = "green !important";
     } else {
        passwordMatch = false;
        message.textContent = "Make sure passwords match."; 
        message.style.color = "red";
        messageContainer.style.borderColor = "red";
-       password1El.style.borderColor = "red !imortant";
-       password2El.style.borderColor = "red !imortant";
+       password1El.style.borderColor = "red !important";
+       password2El.style.borderColor = "red !important";
        return;
     }
 
@@ -46,39 +40,35 @@ const validateForm = () => {
         message.textContent = "Successfully Registered!";
         message.style.color = "green";
         messageContainer.style.borderColor = "green";
-        // password1El.style.borderColor = "green";
-        // password2El.style.borderColor = "green";
     }
 }
 
-const storeFormdata = () => {
+const storeFormData = () => {
     const user = {
         name: form.name.value,
         phone: form.phone.value,
         email: form.email.value,
-        website: form.website.value,
-        password: form.password.value
+        website: form.website.value
     };
 
-    // Do something with user data
+    // Save user data
     console.log(user);
 };
 
-
-
-const processFormData = (e) => {
-    e.preventDefault();
-    // console.log(e);
+const processFormData = (event) => {
+    event.preventDefault();
 
     // Validate Form 
     validateForm();
 
     // Submit Data if valid
     if (isValid && passwordMatch) {
-        storeFormdata();
+        storeFormData();
         form.reset();
     }
 }
 
 // Event Listener
 form.addEventListener("submit", processFormData);
+
+
